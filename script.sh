a1913ef7ab74d08ceddd55c0d9ba37c725a15867
rageshm110 <rageshm110@gmail.com>
2018-02-17 12:08:24 +0000

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "This is going to do a Jenkins integration."
+echo "Hope this works :P"
+mkdir /var/tmp/testDirectory
+mkdir /var/tmp/deleteAfterTest
+cd /var/tmp
+touch abc
+touch def
